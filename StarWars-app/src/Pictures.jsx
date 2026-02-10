b903c5a39a6ee54ee50140b6641c8bc861462b2c
Mikhail Srago <mihanik999@gmail.com>
2026-02-10 20:28:37 +0200

    section Friends updated with friends.map

--- StarWars-app/src/Pictures.jsx
+++ StarWars-app/src/Pictures.jsx
@@ -10,18 +10,14 @@ import friend9 from './assets/friend9.jpg'
 
 
 const Pictures = () => {
+  const friends = [friend1, friend2, friend3, friend4, friend5, friend6, friend7, friend8, friend9];
+
   return (
     <section className="float-end w-50 row ms-2 me-0 border border-warning rounded-bottom-1">
         <h2 className="col-sm-12 text-center">Dream Team</h2>
-        <img src={friend1} alt="Friend 1" className="col-sm-4 p-1" />
-        <img src={friend2} alt="Friend 2" className="col-sm-4 p-1" />
-        <img src={friend3} alt="Friend 3" className="col-sm-4 p-1" />
-        <img src={friend4} alt="Friend 4" className="col-sm-4 p-1" />
-        <img src={friend5} alt="Friend 5" className="col-sm-4 p-1" />
-        <img src={friend6} alt="Friend 6" className="col-sm-4 p-1" />
-        <img src={friend7} alt="Friend 7" className="col-sm-4 p-1" />
-        <img src={friend8} alt="Friend 8" className="col-sm-4 p-1" />
-        <img src={friend9} alt="Friend 9" className="col-sm-4 p-1" />
+        {friends.map((friend, index) => (
+          <img key={index} src={friend} alt={`Friend ${index + 1}`} className="col-sm-4 p-1" />
+        ))}
     </section>
   )
 }
